22d808eb5b77f8eab80adfef8b5624367c8d480f
sumin <dwckey5356@gmail.com>
2026-01-08 15:17:55 +0900

    line organize

--- src/sdk/writer/code_in.ts
+++ src/sdk/writer/code_in.ts
@@ -9,36 +9,30 @@ import {
 import {
     createAnchorProfile,
     createInstructionBuilder,
-    createPinocchioProfile,
     dbCodeInInstruction,
     getCodeAccountPda,
     getDbAccountPda,
     getUserPda,
 } from "../../contract";
 import {
-    DIRECT_METADATA_MAX_BYTES,
-    DEFAULT_IQ_MINT,
     DEFAULT_LINKED_LIST_THRESHOLD,
+    DEFAULT_WRITE_FEE_LAMPORTS,
     DEFAULT_WRITE_FEE_RECEIVER,
 } from "../constants";
-import {DEFAULT_PINOCCHIO_PROGRAM_ID} from "../../contract/constants";
-import {resolveAssociatedTokenAccount} from "../utils/ata";
 import {readMagicBytes} from "../utils/magic_bytes";
-import {DEFAULT_SESSION_SPEED} from "../utils/session_speed";
 import {ensureUserInitialized, sendTx} from "./writer_utils";
 import {uploadLinkedList, uploadSession} from "./uploading_methods";
 
 const IDL = require("../../../idl/code_in.json") as Idl;
-const IQ_MINT = new PublicKey(DEFAULT_IQ_MINT);
+
 
 export async function codein(
     input: { connection: Connection; signer: Signer },
     chunks: string[],
-    isAnchor = false,
+    isAnchor = true,
     filename?: string,
     method = 0,
     filetype = "",
-    speed: string = DEFAULT_SESSION_SPEED,
 ) {
     // Basic validation and input setup
     const totalChunks = chunks.length;
@@ -48,12 +42,7 @@ export async function codein(
     const {connection, signer} = input;
 
     // Program context + PDAs
-    const profile =
-        isAnchor
-            ? createAnchorProfile()
-            : createPinocchioProfile(
-                new PublicKey(DEFAULT_PINOCCHIO_PROGRAM_ID),
-            );
+    const profile = createAnchorProfile();
     const builder = createInstructionBuilder(IDL, profile.programId);
     const user = signer.publicKey;
     const userState = getUserPda(profile, user);
@@ -86,85 +75,57 @@ export async function codein(
     const magic = readMagicBytes(chunks[0]);
     const resolvedFiletype = filetype || magic.mime;
     const safeFilename = filename ?? `${seq}.${magic.ext}`;
-    const metadataPayload = {
+    const metadata = JSON.stringify({
         filetype: resolvedFiletype,
         method,
         filename: safeFilename,
         total_chunks: totalChunks,
-    };
-    const dataPayload = chunks.join("");
-    const directMetadata = JSON.stringify({
-        ...metadataPayload,
-        data: dataPayload,
     });
-    const useDirectPath =
-        Buffer.byteLength(directMetadata, "utf8") <= DIRECT_METADATA_MAX_BYTES;
 
     // Upload chunks (linked-list vs session)
     let onChainPath = "";
-    let metadata = directMetadata;
-    let useSession = false;
-    if (!useDirectPath) {
-        metadata = JSON.stringify(metadataPayload);
-        useSession = totalChunks >= DEFAULT_LINKED_LIST_THRESHOLD;
-        if (!useSession) {
-            onChainPath = await uploadLinkedList(
-                connection,
-                signer,
-                builder,
-                user,
-                codeAccount,
-                chunks,
-                method,
-            );
-        } else {
-            onChainPath = await uploadSession(
-                connection,
-                signer,
-                builder,
-                profile,
-                user,
-                userState,
-                seq,
-                chunks,
-                method,
-                {
-                    speed,
-                },
-            );
-        }
-    }
 
-    // Finalize with db_code_in (fees are handled by the program)
-    const feeReceiver = new PublicKey(DEFAULT_WRITE_FEE_RECEIVER);
-    const sessionAccount = useSession ? new PublicKey(onChainPath) : null;
-    const sessionFinalize = useSession
-        ? {
-            seq: new BN(seq.toString()),
-            total_chunks: totalChunks,
-        }
-        : null;
-    let iqAtaAccount: PublicKey | null | undefined;
-    if (useDirectPath) {
-        iqAtaAccount = await resolveAssociatedTokenAccount(
+    if (totalChunks < DEFAULT_LINKED_LIST_THRESHOLD) {
+        onChainPath = await uploadLinkedList(
             connection,
+            signer,
+            builder,
             user,
-            IQ_MINT,
-            false,
+            codeAccount,
+            chunks,
+            method,
+        );
+    } else {
+        onChainPath = await uploadSession(
+            connection,
+            signer,
+            builder,
+            profile,
+            user,
+            userState,
+            seq,
+            chunks,
+            method,
         );
     }
+
+    // Finalize with fee transfer + db_code_in
+    const feeReceiver = new PublicKey(DEFAULT_WRITE_FEE_RECEIVER);
+    const feeIx = SystemProgram.transfer({
+        fromPubkey: user,
+        toPubkey: dbAccount,
+        lamports: DEFAULT_WRITE_FEE_LAMPORTS, //we can delete it because i changed this to charge from contract , but we need to add ata
+    });
     const dbIx = dbCodeInInstruction(
         builder,
-        {
-            user,
-            db_account: dbAccount,
-            receiver: feeReceiver,
-            system_program: SystemProgram.programId,
-            session: sessionAccount ?? undefined,
-            iq_ata: iqAtaAccount ?? undefined,
-        },
-        {on_chain_path: onChainPath, metadata, session: sessionFinalize},
+        {user, db_account: dbAccount, system_program: SystemProgram.programId},
+        {on_chain_path: onChainPath, metadata, session: null},
     );
+    dbIx.keys.push({
+        pubkey: feeReceiver,
+        isSigner: false,
+        isWritable: true,
+    });
 
-    return sendTx(connection, signer, dbIx);
+    return sendTx(connection, signer, [feeIx, dbIx]);
 }
